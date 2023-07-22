279169d52dac87da69dc044b2bbd7471d5314d6b
Aliya Kanani <aliyakanani@Aliyas-MacBook-Pro.local>
2023-07-21 20:44:28 -0400

    implement interviewer list functionality

---
+++ src/components/InterviewerListItem.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import "components/InterviewerListItem.scss";
+
+export default function InterviewerListItem(props) {
+  return (
+    <li
+      className={`interviewers__item ${props.selected ? 'interviewers__item--selected' : ''}`}
+      onClick={props.setInterviewer}
+    >
+      <img
+        className="interviewers__item-image"
+        src={props.avatar}
+        alt={props.name}
+      />
+      {props.selected && props.name}
+    </li>
+  );
+}
